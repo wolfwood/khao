68996a1cdbf062c791b50e794112783a71aac660
wolfwood <wolfwood@xomb.org>
2020-10-06 22:44:25 -0700

    discover API urls dynamically from the root

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use tokio;
 use serde_json;
 
 use std::fs::File;
+
+// Traits
 use std::io::Write;
 
 use anyhow::Result;
@@ -17,7 +19,25 @@ async fn main() -> Result<()> {
 
             serde_json::from_reader(file)?
         } else {
-            let body = reqwest::get("https://api.mmoui.com/v4/game/ESO/filelist.json").await?;
+            let root_json : serde_json::Value = reqwest::get("https://api.mmoui.com/v4/globalconfig.json").await?.json().await?;
+
+            let gameconfig_url = &*root_json.get("games").unwrap().get(1).unwrap().get("gameConfig").unwrap();
+
+            if gameconfig_url.as_str().unwrap() != "https://api.mmoui.com/v4/game/ESO/gameconfig.json" {
+                eprintln!("game config url changed: {}", gameconfig_url);
+            }
+
+            let gameconfig_json : serde_json::Value = reqwest::get(gameconfig_url.as_str().unwrap()).await?.json().await?;
+
+
+            let filelist_url = &*gameconfig_json.get("apiFeeds").unwrap().get("fileList").unwrap();
+
+            if filelist_url.as_str().unwrap() != "https://api.mmoui.com/v4/game/ESO/filelist.json" {
+                println!("filelist url changed: {}", filelist_url);
+            }
+
+
+            let body = reqwest::get(filelist_url.as_str().unwrap()).await?;
 
             let mut file = File::create(cache)?;
 
