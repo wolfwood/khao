290e4cb491cb1c29cadee527db27efda7395f7b9
Eric Conlon <ejconlon@gmail.com>
2020-10-08 10:45:57 -0700

    Add serde structs, format, etc

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,105 @@
 use reqwest;
-use tokio;
 use serde_json;
-
-use std::fs::File;
-
-// Traits
-use std::io::Write;
+use tokio;
 
 use anyhow::Result;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
+const GLOBAL_CONFIG_URL: &str = "https://api.mmoui.com/v4/globalconfig.json";
+const ESO_GAME_ID: &str = "ESO";
+const GAME_CONFIG_URL: &str = "https://api.mmoui.com/v4/game/ESO/gameconfig.json";
+const FILE_LIST_URL: &str = "https://api.mmoui.com/v4/game/ESO/filelist.json";
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Game {
+    #[serde(alias = "gameID")]
+    game_id: String,
+    game_config: String,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct GlobalConfig {
+    games: Vec<Game>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct ApiFeeds {
+    file_list: String,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct GameConfig {
+    api_feeds: ApiFeeds,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct FileInfo {}
+
+fn read_cache(path: &Path) -> Result<Vec<FileInfo>> {
+    let file = File::open(path)?;
+    let val = serde_json::from_reader(&file)?;
+    Ok(val)
+}
+
+async fn write_cache(path: &Path) -> Result<Vec<FileInfo>> {
+    let global_config: GlobalConfig = reqwest::get(GLOBAL_CONFIG_URL).await?.json().await?;
+
+    let game = global_config
+        .games
+        .iter()
+        .find(|g| g.game_id == ESO_GAME_ID)
+        .expect("No ESO def found");
+    let game_config_url = &game.game_config;
+
+    if game_config_url != GAME_CONFIG_URL {
+        eprintln!("Game config url changed: {}", game.game_config);
+    }
+
+    let game_config: GameConfig = reqwest::get(game_config_url).await?.json().await?;
+
+    eprintln!(
+        "Game config: {}",
+        serde_json::to_string_pretty(&game_config)?
+    );
+
+    let file_list_url = game_config.api_feeds.file_list;
+
+    if file_list_url != FILE_LIST_URL {
+        eprintln!("Filelist url changed: {}", file_list_url);
+    }
+
+    let res = reqwest::get(&file_list_url).await?;
+
+    let body = res.text().await?;
+
+    let file_list: Vec<FileInfo> = serde_json::from_str(&body)?;
+
+    let mut file = File::create(path)?;
+
+    file.write_all(body.as_bytes())?;
+
+    Ok(file_list)
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let cache = std::path::Path::new("mycache.data");
+    let cache = Path::new("mycache.data");
 
-    let json : serde_json::Value =
-        if cache.exists() {
-            let mut file = File::open(cache)?;
+    let file_list = if cache.exists() {
+        read_cache(&cache)?
+    } else {
+        write_cache(&cache).await?
+    };
 
-            serde_json::from_reader(file)?
-        } else {
-            let root_json : serde_json::Value = reqwest::get("https://api.mmoui.com/v4/globalconfig.json").await?.json().await?;
-
-            let gameconfig_url = &*root_json.get("games").unwrap().get(1).unwrap().get("gameConfig").unwrap();
-
-            if gameconfig_url.as_str().unwrap() != "https://api.mmoui.com/v4/game/ESO/gameconfig.json" {
-                eprintln!("game config url changed: {}", gameconfig_url);
-            }
-
-            let gameconfig_json : serde_json::Value = reqwest::get(gameconfig_url.as_str().unwrap()).await?.json().await?;
-
-
-            let filelist_url = &*gameconfig_json.get("apiFeeds").unwrap().get("fileList").unwrap();
-
-            if filelist_url.as_str().unwrap() != "https://api.mmoui.com/v4/game/ESO/filelist.json" {
-                println!("filelist url changed: {}", filelist_url);
-            }
-
-
-            let body = reqwest::get(filelist_url.as_str().unwrap()).await?;
-
-            let mut file = File::create(cache)?;
-
-            let body = body.text().await?;
-
-            file.write_all(body.as_bytes())?;
-
-            serde_json::from_str(&body)?
-        };
-
-    println!("{}", serde_json::to_string_pretty(&json)?);
+    println!("{}", serde_json::to_string_pretty(&file_list)?);
 
     Ok(())
 }
